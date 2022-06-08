e8677bcfe572f5925461541a5390f277a07f8c8c
Luís Pitta Grós <luispittagroz@gmail.com>
2022-06-08 03:20:17 +0100

    Add gmaps api key to .env, use hooks

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_BOUTIQUES_API_URL: string;
   readonly VITE_BOUTIQUES_ENDPOINT: string;
+  readonly VITE_GMAPS_API_KEY: string;
 }
 
 interface ImportMeta {
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,19 @@
 import Map from '@/components/Map';
+import { Wrapper } from '@googlemaps/react-wrapper';
+import useGeoLocation from '@/hooks/useGeoLocation';
+import useNearbyBoutiques from '@/hooks/useNearbyBoutiques';
 
 const Home = () => {
-  return <Map />;
+  const [userPosition, supports] = useGeoLocation();
+  const nearbyBoutiques = useNearbyBoutiques(userPosition);
+
+  console.log(nearbyBoutiques);
+
+  return (
+    <Wrapper apiKey={import.meta.env.VITE_GMAPS_API_KEY}>
+      <Map />
+    </Wrapper>
+  );
 };
 
 export default Home;

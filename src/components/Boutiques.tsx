c9790abfadc28b11a71bbc406b52ab6ebdccf9c9
Luís Pitta Grós <luispittagroz@gmail.com>
2022-06-08 05:11:24 +0100

    Implement boutiques component

--- src/components/Boutiques.tsx
+++ src/components/Boutiques.tsx
@@ -1,13 +1,45 @@
 import { FC } from 'react';
-import useGeoLocation from '@/hooks/useGeoLocation';
 import useNearbyBoutiques from '@/hooks/useNearbyBoutiques';
 import Map from '@/components/Map';
+import { useLoadScript } from '@react-google-maps/api';
+import '@/components/Boutiques.scss';
 
-const Boutiques: FC = () => {
-  const [userPosition, supports] = useGeoLocation();
-  const [boutiques, loading] = useNearbyBoutiques(userPosition);
+const libraries = ['geometry'] as (
+  | 'geometry'
+  | 'drawing'
+  | 'localContext'
+  | 'places'
+  | 'visualization'
+)[];
 
-  return <Map boutiques={boutiques} userPosition={userPosition} />;
+interface BoutiquesProps {
+  userPosition?: UserPosition;
+  distance?: number;
+}
+
+const Boutiques: FC<BoutiquesProps> = ({ userPosition, distance }) => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_GMAPS_API_KEY,
+    libraries,
+  });
+
+  const [boutiques, loading] = useNearbyBoutiques(userPosition, distance);
+
+  if (!isLoaded) return <div>Loading...</div>;
+
+  return (
+    <>
+      {!boutiques?.length && !loading && (
+        <p className="no-boutiques">No boutiques found near you</p>
+      )}
+      <Map boutiques={boutiques} userPosition={userPosition} />;
+    </>
+  );
+};
+
+Boutiques.defaultProps = {
+  userPosition: undefined,
+  distance: undefined,
 };
 
 export default Boutiques;

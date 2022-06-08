5af683810d17f9f80cd0f8e0483c83b9fe150ff8
Luís Pitta Grós <luispittagroz@gmail.com>
2022-06-08 04:07:47 +0100

    Use react maps, implement map component with markers

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,35 @@
-const Map = () => {
+import { GoogleMap, Marker } from '@react-google-maps/api';
+import { FC } from 'react';
+
+import '@/components/Map.scss';
+
+interface MapProps {
+  boutiques?: Boutique[];
+  userPosition?: UserPosition;
+}
+
+const Map: FC<MapProps> = ({ boutiques, userPosition }) => {
+  if (!userPosition || !boutiques?.length) return null;
+
+  const latLng = (latitude: number, longitude: number) =>
+    new google.maps.LatLng(latitude, longitude);
+
   return (
-    <div>
-      <span />
-    </div>
+    <GoogleMap
+      id="map"
+      zoom={10}
+      center={latLng(userPosition.latitude, userPosition.longitude)}
+    >
+      {boutiques.map(({ _id: id, location: { lat, lon } }) => (
+        <Marker key={id} position={latLng(lat, lon)} />
+      ))}
+    </GoogleMap>
   );
+};
+
+Map.defaultProps = {
+  boutiques: undefined,
+  userPosition: undefined,
 };
 
 export default Map;

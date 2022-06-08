9993f6ec4d972311f72299cd91e5e57e48cff80c
Luís Pitta Grós <luispittagroz@gmail.com>
2022-06-08 03:41:51 +0100

    Use state to save user position, add accuracy property

--- src/hooks/useGeoLocation.ts
+++ src/hooks/useGeoLocation.ts
@@ -1,9 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const useGeoLocation = (): [UserPosition | undefined, boolean] => {
   const [supports, setSupports] = useState(false);
-
-  const userPosition = useRef<UserPosition>();
+  const [position, setPosition] = useState<UserPosition | undefined>(undefined);
 
   useEffect(() => {
     setSupports('geolocation' in navigator);
@@ -11,20 +10,19 @@ const useGeoLocation = (): [UserPosition | undefined, boolean] => {
 
   useEffect(() => {
     if (supports) {
-      const geo = navigator.geolocation;
-
       const options = {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0,
       };
 
-      geo.getCurrentPosition(
-        (position) => {
-          userPosition.current = {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          };
+      navigator.geolocation.getCurrentPosition(
+        ({ coords: { latitude, longitude, accuracy } }) => {
+          setPosition({
+            latitude,
+            longitude,
+            accuracy,
+          });
         },
         console.log,
         options,
@@ -32,7 +30,7 @@ const useGeoLocation = (): [UserPosition | undefined, boolean] => {
     }
   }, [supports]);
 
-  return [userPosition.current, supports];
+  return [position, supports];
 };
 
 export default useGeoLocation;
